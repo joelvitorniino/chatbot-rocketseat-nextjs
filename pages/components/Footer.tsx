a17d7e2943723ff62c18dd5c7616d65d2d74fa6f
joelvitorniino <joelprofessor13@gmail.com>
2023-01-07 16:04:57 -0300

    modify color of background of footer

--- pages/components/Footer.tsx
+++ pages/components/Footer.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Footer() {
     return (
-        <footer className="flex items-center justify-center bg-blackOpaque border-t-1 border-solid border-blackLight text-white">
+        <footer className="flex items-center justify-center bg-gray-500 border-t-1 border-solid border-blackLight text-white">
             <p>
                 <Link href="https://github.com/joelvitorniino"><span className="font-bold">Joel Vitor</span> &copy; 2023</Link>
             </p>
