29cefd2537cac379877eb6fb9956c0e7eb07cbae
joelvitorniino <joelprofessor13@gmail.com>
2023-01-07 17:36:35 -0300

    about page

---
+++ pages/about.tsx
@@ -0,0 +1,8 @@
+export default function About() {
+    return (
+        <div className="items-center">
+            <h1 className="text-2xl mb-4 text-center">About this project:</h1>
+            <p className="mb-6 ml-0 mr-auto mt-6 text-center">Lorem ipsum dolor</p>
+        </div>
+    )
+}
