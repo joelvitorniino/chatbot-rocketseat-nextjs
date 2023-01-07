45823af49d45ea472a7846d8a56e3bc2a61fe2ae
joelvitorniino <joelprofessor13@gmail.com>
2023-01-07 16:56:13 -0300

    component layout

---
+++ pages/components/Layout.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+
+export default function Layout({ children }: React.PropsWithChildren) {
+    return (
+        <>
+            <Navbar />
+            <main className="main-container">{ children }</main>
+            <Footer />
+        </>
+    )
+}
