bf64a2f520d81224fe3bb0635f3565a37292de63
joelvitorniino <joelprofessor13@gmail.com>
2023-01-09 13:30:48 -0300

    add hover in about and footer

--- pages/components/Footer.tsx
+++ pages/components/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
     return (
         <footer className="flex items-center justify-center bg-gray-500 border-t-1 border-solid border-blackLight text-white">
             <p>
-                <Link href="https://github.com/joelvitorniino"><span className="font-bold">Joel Vitor</span> &copy; 2023</Link>
+                <Link href="https://github.com/joelvitorniino"><span className="font-bold hover:text-gray-300">Joel Vitor</span> &copy; 2023</Link>
             </p>
         </footer>
     )
