a9f3758cca61a3c7313368cd7ff6938e6c7385fb
joelvitorniino <joelprofessor13@gmail.com>
2023-01-10 13:44:22 -0300

    add hover in chat and navbar

--- pages/chat.tsx
+++ pages/chat.tsx
@@ -5,7 +5,7 @@ export default function Chat() {
                 <div className="w-600 h-400 ml-5 mr-0 border border-solid border-whiteLight p-5 overflow-y-auto"></div>
                 <input type="text" name="name" className="w-600 border border-solid border-whiteLight h-12 pl-0 pr-5 text-sm" placeholder="Digite seu nome:" />
                 <input type="text" name="message" className="w-600 border border-solid border-whiteLight h-12 pl-0 pr-5 text-sm" placeholder="Digite sua mensagem:" />
-                <button type="submit" className="w-600 h-12 text-sm bg-blueLight text-center leading-md font-bold text-white mt-3">Enviar</button>
+                <button className="w-600 h-12 text-sm bg-blueLight text-center leading-md font-bold text-white mt-3 hover:text-gray-300">Enviar</button>
             </form>
         <br />
         </div> 
