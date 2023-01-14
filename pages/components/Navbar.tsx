c85c0ea4ca82893857756cd7fe3489453cb27c91
joelvitorniino <joelprofessor13@gmail.com>
2023-01-14 14:49:50 -0300

    implement register to api

--- pages/components/Navbar.tsx
+++ pages/components/Navbar.tsx
@@ -1,5 +1,4 @@
 import Link from "next/link";
-
 export default function Navbar() {
 
   return (
--- pages/register.tsx
+++ pages/register.tsx
@@ -1,7 +1,23 @@
 import Head from "next/head";
 import Link from "next/link";
+import { useState } from "react";
 
 export default function Register() {
+  const [ name, setName ] = useState("");
+  const [ email, setEmail ] = useState("");
+  const [ password, setPassword ] = useState("");
+
+  const register = () => {
+    fetch('http://localhost:3001/api/v1/register', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ name, email, password })
+    })
+  };
+
   return (
     <>
       <Head>
@@ -17,7 +33,7 @@ export default function Register() {
 
           <div className="bg-white shadow-md rounded-md p-6 space-y-4">
             <div>
-              <form action="" method="POST">
+              <form method="POST">
                 <label
                   htmlFor="name"
                   className="block text-sm text-gray-800 mb-2"
@@ -28,6 +44,8 @@ export default function Register() {
                   type="text"
                   name="name"
                   className="w-full bg-gray-50 border border-gray-300 dark:bg-gray-800 dark:text-gray-500 focus:border-blue-500 focus:ring-2 focus:ring-blue-500 focus:ring-opacity-20 rounded px-3 py-2 text-sm text-gray-800 placeholder-gray-300 focus:outline-none transition duration-200 ease-in-out"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
                 />
 
                 <label
@@ -40,6 +58,8 @@ export default function Register() {
                   type="text"
                   name="email"
                   className="w-full bg-gray-50 border border-gray-300 dark:bg-gray-800 dark:text-gray-500 focus:border-blue-500 focus:ring-2 focus:ring-blue-500 focus:ring-opacity-20 rounded px-3 py-2 text-sm text-gray-800 placeholder-gray-300 focus:outline-none transition duration-200 ease-in-out"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
                 />
 
                 <div>
@@ -50,14 +70,16 @@ export default function Register() {
                     Password
                   </label>
                   <input
-                    type="text"
+                    type="password"
                     name="password"
                     className="w-full bg-gray-50 border border-gray-300 dark:bg-gray-800 dark:text-gray-500 focus:border-blue-500 focus:ring-2 focus:ring-blue-500 focus:ring-opacity-20 rounded px-3 py-2 text-sm text-gray-800 placeholder-gray-300 focus:outline-none transition duration-200 ease-in-out"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
                   />
                 </div>
 
                 <div>
-                  <button className="w-full text-sm bg-red-400 hover:bg-red-500 dark:bg-gray-800 dark:hover:bg-gray-900 px-6 py-1 mt-3 rounded text-white shadow">
+                  <button className="w-full text-sm bg-red-400 hover:bg-red-500 dark:bg-gray-800 dark:hover:bg-gray-900 px-6 py-1 mt-3 rounded text-white shadow" onClick={register}>
                     <Link href="#">Register</Link>
                   </button>
                 </div>
