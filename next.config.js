22779b6dbd043707ab785a0c7fda9852e88593ba
joelvitorniino <joelprofessor13@gmail.com>
2023-01-29 17:04:46 -0300

    support environment variables on nextjs

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
+
+require('dotenv').config();
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  env: {
+    baseUrl: process.env.BASE_URL
+  }
+};
 
 module.exports = nextConfig
