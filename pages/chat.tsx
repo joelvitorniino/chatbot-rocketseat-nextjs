56661db4501f7e798782337a6df49fd32968cdc5
joelvitorniino <joelprofessor13@gmail.com>
2023-01-31 16:02:51 -0300

    updated url

--- pages/chat.tsx
+++ pages/chat.tsx
@@ -4,7 +4,7 @@ import React, { useEffect, useState } from "react";
 import { io } from "socket.io-client";
 import { api } from "../lib/axios";
 
-const socket = io("localhost:3001", {
+const socket = io("chatbot-rocketseat-api-production.up.railway.app", {
   transports: ["websocket"],
   forceNew: true,
 });
