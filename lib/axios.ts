087402b8cfaae33448166403504d86ee495a9226
joelvitorniino <joelprofessor13@gmail.com>
2023-02-01 16:46:39 -0300

    switch http to https

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: 'http://chatbot-rocketseat-api-production.up.railway.app/'
+    baseURL: 'https://chatbot-rocketseat-api-production.up.railway.app/'
 });
