edc60199868fcde20b8db666a2018027079437a0
joelvitorniino <joelprofessor13@gmail.com>
2023-02-02 16:28:10 -0300

    add responsive to chat

--- pages/chat.tsx
+++ pages/chat.tsx
@@ -24,73 +24,73 @@ export default function Chat() {
     socket.emit("sendMessage", { author, message });
   };
 
-  useEffect(() => {
-    socket.on("connection", null as any);
-    socket.on("previousMessages", (msg) => {
-      msg.forEach((messages: Message) => {
-        setMessages((currentMsg) => [
-          ...currentMsg,
-          { author: messages.author, message: messages.message },
-        ]);
-      });
-    });
+  // useEffect(() => {
+  //   socket.on("connection", null as any);
+  //   socket.on("previousMessages", (msg) => {
+  //     msg.forEach((messages: Message) => {
+  //       setMessages((currentMsg) => [
+  //         ...currentMsg,
+  //         { author: messages.author, message: messages.message },
+  //       ]);
+  //     });
+  //   });
 
-    socket.on("receivedMessage", (msg) => {
-      setMessages((currentMsg) => [
-        ...currentMsg,
-        { author: msg.author, message: msg.message },
-      ]);
-    });
+  //   socket.on("receivedMessage", (msg) => {
+  //     setMessages((currentMsg) => [
+  //       ...currentMsg,
+  //       { author: msg.author, message: msg.message },
+  //     ]);
+  //   });
 
-    getUser();
+  //   getUser();
 
-    return function cleanup() {
-      socket.removeListener("previousMessages");
-      socket.removeListener("receivedMessage");
-    };
-  }, []);
+  //   return function cleanup() {
+  //     socket.removeListener("previousMessages");
+  //     socket.removeListener("receivedMessage");
+  //   };
+  // }, []);
 
-  useEffect(() => {
-    verifyLogin();
-  }, [message]);
+  // useEffect(() => {
+  //   verifyLogin();
+  // }, [message]);
 
-  const verifyLogin = () => {
-    const bearer_token = `Bearer ${localStorage.getItem("access_token")}`;
+  // const verifyLogin = () => {
+  //   const bearer_token = `Bearer ${localStorage.getItem("access_token")}`;
 
-    api
-      .get("/api/register", {
-        headers: {
-          Authorization: bearer_token,
-        },
-      })
-      .then(() => {
-        return;
-      })
-      .catch((e) => {
-        router.push({
-          pathname: "/",
-        });
+  //   api
+  //     .get("/api/register", {
+  //       headers: {
+  //         Authorization: bearer_token,
+  //       },
+  //     })
+  //     .then(() => {
+  //       return;
+  //     })
+  //     .catch((e) => {
+  //       router.push({
+  //         pathname: "/",
+  //       });
 
-        console.log(e);
-      });
-  };
+  //       console.log(e);
+  //     });
+  // };
 
-  const getUser = () => {
-    api
-      .post("/api/register/getUser", {
-        email: localStorage.getItem("email"),
-      })
-      .then((res) => {
-        setAuthor(res.data.username);
-      })
-      .catch((e) => {
-        router.push({
-          pathname: "/",
-        });
+  // const getUser = () => {
+  //   api
+  //     .post("/api/register/getUser", {
+  //       email: localStorage.getItem("email"),
+  //     })
+  //     .then((res) => {
+  //       setAuthor(res.data.username);
+  //     })
+  //     .catch((e) => {
+  //       router.push({
+  //         pathname: "/",
+  //       });
 
-        console.log(e);
-      });
-  };
+  //       console.log(e);
+  //     });
+  // };
 
   return (
     <>
@@ -102,20 +102,20 @@ export default function Chat() {
           <input
             type="text"
             name="message"
-            className="w-600 border border-solid border-whiteLight h-12 pl-0 pr-5 text-sm hover:border-blue-600 focus:ring-2 focus:ring-blue-500 focus:ring-opacity-20 placeholder-gray-300 focus:outline-none transition duration-200 ease-in-out"
+            className="w-full border border-solid border-whiteLight h-12 pl-0 pr-5 text-sm hover:border-blue-600 focus:ring-2 focus:ring-blue-500 focus:ring-opacity-20 placeholder-gray-300 focus:outline-none transition duration-200 ease-in-out"
             placeholder=" Digite sua mensagem:"
             value={message}
             onChange={(e) => setMessage(e.target.value)}
           />
 
           <button
-            className="w-600 h-12 text-sm bg-blueLight text-center leading-md font-bold text-white mt-3 hover:text-gray-300"
+            className="w-full h-12 text-sm bg-blueLight text-center leading-md font-bold text-white mt-3 hover:text-gray-300"
             onClick={handleMessages}
           >
             Enviar
           </button>
 
-          <div className="w-600 h-400 ml-5 mr-0 border border-solid border-whiteLight p-5 overflow-y-auto">
+          <div className="w-full h-400 ml-5 mr-0 border border-solid border-whiteLight p-5 overflow-y-auto">
             {messages.map((msg, i) => {
               return (
                 <strong key={i}>
