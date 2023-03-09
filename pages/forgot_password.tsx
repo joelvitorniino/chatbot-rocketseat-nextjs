9777e34b8343c5a9219e797dcd33a209763daf15
joelvitorniino <joelprofessor13@gmail.com>
2023-03-09 15:43:45 -0300

    feat: required labels forgot

--- pages/forgot_password.tsx
+++ pages/forgot_password.tsx
@@ -43,6 +43,7 @@ export default function ForgotPassword() {
                 name="email"
                 className="w-full bg-gray-50 border border-gray-300 dark:bg-gray-800 dark:text-gray-500 focus:border-blue-500 focus:ring-2 focus:ring-blue-500 focus:ring-opacity-20 rounded px-3 py-2 text-sm text-gray-800 placeholder-gray-300 focus:outline-none transition duration-200 ease-in-out"
                 value={email}
+                required
                 onChange={(e) => setEmail(e.target.value)}
               />
               <div>
